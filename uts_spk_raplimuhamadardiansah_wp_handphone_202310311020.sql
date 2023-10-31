bfa89e31e38a2da28ab56128db885a68ffb390ba
Rafly-2002 <138590842+Rafly-2002@users.noreply.github.com>
2023-10-31 10:46:30 +0700

    Add files via upload

---
+++ uts_spk_raplimuhamadardiansah_wp_handphone_202310311020.sql
@@ -0,0 +1,11 @@
+INSERT INTO "07TPLP015".uts_spk_raplimuhamadardiansah_wp_handphone ("Nama Handphone",ram,"Memori Internal",kamera,baterai,"Harga (Rp)",alternatif,"Nilai Akhir","Range") VALUES
+	 ('Samsung J1 Ace','512 MB','4 GB','5 MP','1900 MAH','1,300,000',1.1930473,0.05713644,9),
+	 ('Samsung J1 Mini','768 MB','8 GB','5 MP','1500 MAH','1,125,000',2.0983665,0.100493245,3),
+	 ('Samsung A31','4 GB','32 GB','16 MP','3000 MAH','7,699,000',1.8322446,0.087748356,7),
+	 ('Samsung A05','4 GB','64 GB','50 MP','5000 MAH','1,499,000',1.979313,0.09479163,6),
+	 ('Samsung A04 E','36 GB','32 GB','13 MP','5000 MAH','1,159,000',2.4023085,0.115049385,2),
+	 ('Samsung A54','12 GB','512 GB','200 MP','6800 MAH','1,230,000',3.3993206,0.16279747,1),
+	 ('Redmi 12','8 GB','64 GB','50 MP','5000 MAH','1,499,000',2.1139183,0.101238035,4),
+	 ('Redmi 12 C',' 3 GB','32 GB','50 MP','5000 MAH','1,199,000',2.104092,0.10076744,5),
+	 ('Redmi 10 A','3 GB','32 GB','13 MP','5000 MAH','1,099,000',2.0589364,0.09860488,8),
+	 ('Redmi Note 12','4 GB','128 GB','50 MP','5000 MAH','2,079,000',1.6991255,0.081373125,10);
